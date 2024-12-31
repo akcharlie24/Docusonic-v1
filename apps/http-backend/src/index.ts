1b65dad1001bb80af03dcd075697a63fa30d8449
Akshat Bhardwaj <akshatbhardwaj24@gmail.com>
2024-12-31 13:16:07 +0530

    chore: added some code

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,6 +1,7 @@
 // TODO: write a cron job (every 5 sec update the sheet)
 // TODO: ws server -> intermediate queue while doing batch updates (to update the db (content in the Sheet))
 // TODO: frontend -> janky function
+// tiptap / quilt / (bare-metal approach)
 
 import express, { Request, Response } from "express";
 import { prismaClient } from "@repo/prisma/client";
@@ -35,6 +36,22 @@ app.post("/signin", async (req: Request, res: Response) => {
     where: {
       username: username,
     },
+  });
+
+  if (!user) {
+    res.status(404).json({ message: "No user exists, try different usename" });
+    return;
+  }
+
+  const savedPass = user.password;
+
+  bcrypt.compare(savedPass, password, (err) => {
+    if (err) {
+      res.status(404).json({ message: "Wrong password please try again" });
+      return;
+    }
+
+    res.status(200).json({ message: "Logged in" });
   });
 });
 
